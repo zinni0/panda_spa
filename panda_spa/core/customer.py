592fe7bb40238c95a638acc2995f63d69a530bb4
Richard Zinecker <zinecker@miracode.io>
2026-01-20 16:50:11 +0100

    fix linter error

--- panda_spa/core/customer.py
+++ panda_spa/core/customer.py
@@ -2,7 +2,7 @@ class Customer:
     _id_counter: int = 1
 
     def __init__(self, name: str, species: str):
-        self.id = Customer._id_counter
+        self.customer_id = Customer._id_counter
         Customer._id_counter += 1
         self.name = name
         self.species = species
