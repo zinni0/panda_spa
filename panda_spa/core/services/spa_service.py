26df531b295f8e975ec1e0e9b6d2f154bbe08add
Richard Zinecker <zinecker@miracode.io>
2026-03-01 19:12:48 +0100

    added thermal bath

--- panda_spa/core/services/spa_service.py
+++ panda_spa/core/services/spa_service.py
@@ -1,5 +1,30 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Any
+
 from panda_spa.validation.metaclasses import ServiceRegistryMeta
 
 
-class SpaService(metaclass=ServiceRegistryMeta):
-    pass
+class SpaService(ABC, metaclass=ServiceRegistryMeta):
+    """
+    Abstract class for all Spa-Services
+    """
+
+    name: str
+    price: float
+    duration: int
+
+    def __init__(self, price: float, duration: int) -> None:
+        self.price = price
+        self.duration = duration
+
+    @abstractmethod
+    def get_description(self) -> str:
+        """Must be implemented from every Service"""
+        pass
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "service_type": self.__class__.__name__,
+            "price": self.price,
+            "duration": self.duration
+        }
--- panda_spa/core/services/thermal_bath.py
+++ panda_spa/core/services/thermal_bath.py
@@ -1,5 +1,24 @@
+from typing import Dict, Any
+
 from panda_spa.core.services.spa_service import SpaService
+from panda_spa.validation.descriptors import RangeValueDescriptor
 
 
 class ThermalBath(SpaService):
-    pass
+    temperature = RangeValueDescriptor(
+        name="temperature",
+        min_value=30,
+        max_value=40
+    )
+
+    def __init__(self, price: float, duration: int, temperature: int):
+        super().__init__(price=price, duration=duration)
+        self.temperature = temperature
+
+    def get_description(self) -> str:
+        return f"Thermal bath at {self.temperature}°C"
+
+    def to_dict(self) -> Dict[str, Any]:
+        base = super().to_dict()
+        base["temperature"] = self.temperature
+        return base
