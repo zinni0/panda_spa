5a3925c4319fea24d5410bde06d7914ea19a9e1b
Richard Zinecker <zinecker@miracode.io>
2026-03-13 15:58:33 +0100

    pylint errors fix

--- panda_spa/schema/finance.py
+++ panda_spa/schema/finance.py
@@ -11,10 +11,11 @@ class FinanceSchema(BaseModel):
     date: datetime = Field(default_factory=datetime.utcnow)
 
     @validator("type")
-    def type_must_be_valid(cls, v):
-        if v not in ("income", "expense"):
+    def type_must_be_valid(cls, value): # pylint: disable=no-self-argument
+        if value not in ("income", "expense"):
+            # pylint: disable=broad-exception-raised
             raise ValueError("Typ muss 'income' oder 'expense' sein")
-        return v
+        return value
 
     class Config:
         from_attributes = True
