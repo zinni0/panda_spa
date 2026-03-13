dadaf55e349ffaa1b05e7b0eaded251dae26db1d
Richard Zinecker <zinecker@miracode.io>
2026-03-13 16:19:57 +0100

    pylint errors fix

--- panda_spa/validation/metaclasses.py
+++ panda_spa/validation/metaclasses.py
@@ -4,8 +4,8 @@ from abc import ABCMeta
 class ServiceRegistryMeta(ABCMeta):
     registry = {}
 
-    def __new__(cls, name, bases, dct):
-        cls_obj = super().__new__(cls, name, bases, dct)
+    def __new__(mcs, name, bases, dct):
+        cls_obj = super().__new__(mcs, name, bases, dct)
         if name != "SpaService":
             ServiceRegistryMeta.registry[name] = cls_obj
         return cls_obj
