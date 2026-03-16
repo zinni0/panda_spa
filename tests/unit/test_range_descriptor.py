f8db495e4dff530133469c64cd6e63173fc2f2d2
Richard Zinecker <zinecker@miracode.io>
2026-03-16 20:15:13 +0100

    updated pytest

--- tests/unit/test_range_descriptor.py
+++ tests/unit/test_range_descriptor.py
@@ -1,15 +1,14 @@
 from panda_spa.validation.descriptors import RangeValueDescriptor
 
 
-class Dummy:
-    value = RangeValueDescriptor("test.range")
-
-
 def test_valid_value(monkeypatch):
     monkeypatch.setattr(
         "panda_spa.validation.descriptors.ConfigLoader.get",
         lambda path: {"min": 0, "max": 10}
     )
+
+    class Dummy:
+        value = RangeValueDescriptor("test.range")
 
     obj = Dummy()
     obj.value = 5
