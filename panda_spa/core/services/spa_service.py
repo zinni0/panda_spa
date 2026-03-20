02483c46537f3ca54da1ae3e62c31e54279a1c98
Richard Zinecker <zinecker@miracode.io>
2026-03-20 12:28:12 +0100

    updated services

--- panda_spa/core/services/spa_service.py
+++ panda_spa/core/services/spa_service.py
@@ -13,26 +13,19 @@ class SpaService(ABC, metaclass=ServiceRegistryMeta):
     - get_description()
     """
 
-    def __init__(self, price: float, cost: float, duration: int) -> None:
+    def __init__(self, price: float, duration: int) -> None:
         if price < 0:
             raise ValueError("price must be non-negative")
-        if cost < 0:
-            raise ValueError("cost must be non-negative")
         if duration <= 0:
             raise ValueError("duration must be positive")
 
         self.price = price
-        self.cost = cost
         self.duration = duration
 
     @property
-    @abstractmethod
     def name(self) -> str:
         """Return the name of the service"""
-
-    @property
-    def profit(self) -> float:
-        return self.price - self.cost
+        return type(self).__name__
 
     @abstractmethod
     def get_description(self) -> str:
@@ -44,6 +37,5 @@ class SpaService(ABC, metaclass=ServiceRegistryMeta):
             "service_type": type(self).__name__,
             "name": self.name,
             "price": self.price,
-            "cost": self.cost,
             "duration": self.duration,
         }
